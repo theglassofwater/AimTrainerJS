713a5cdd946b666737505a2e958efdd3e625ab49
Youssef <joeace2002@gmail.com>
2024-12-03 23:55:07 +0000

    added cubes respaqning on click and colour change on hover

--- src/main.js
+++ src/main.js
@@ -22,8 +22,6 @@ renderer.setSize(
 document.body.appendChild( renderer.domElement );
 
 // setting up cubes
-const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000});
 
 function generateRandNum(min, max) {
     let randomNum = (Math.random() * (max - min) + min).toFixed(3);
@@ -33,9 +31,12 @@ function generateRandNum(min, max) {
 const numOfCubes = 3;
 const cubes = [];
 for (let i = 0; i < numOfCubes; i++){
+    const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+    const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000});
     const cube = new THREE.Mesh( cubeGeometry, cubeMaterial);
     cube.position.x = generateRandNum(-3,3);
     cube.position.y = generateRandNum(-3,3)
+    cube.name= "cube" + i;
     cubes.push(cube);
     scene.add(cubes[i]);
 };
@@ -48,6 +49,9 @@ const raycaster = new THREE.Raycaster();
 function onMouseMove(event) {
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    const intersects = raycaster.intersectObjects(scene.children, true);
+
     
 }
 
@@ -55,37 +59,46 @@ function onClick(event) {
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
-    console.log('Mesh clicked at:', mouse.x, mouse.y);
+    const intersects = raycaster.intersectObjects(scene.children, true);
+
+    for (let i = 0; i < intersects.length; i++){
+        intersects[i].object.position.x = generateRandNum(-3,3);
+        intersects[i].object.position.y = generateRandNum(-3,3);
+        counter++;
+        console.log(counter);
+        
+    };
+
+    console.log('clicked at:', mouse.x, mouse.y);
 }
 
 window.addEventListener('mousemove', onMouseMove);
 window.addEventListener('click', onClick);
 
+let counter = 0;
 
 // game loop
 function animate() {
     requestAnimationFrame(animate);
 
-    const counter = 1;
+    // let isClicked = false
+
     raycaster.setFromCamera(mouse, camera);
-    const intersects = raycaster.intersectObjects(cubes);
-
-    if (intersects.length > 0) {
-        intersects[0].object.material.color.set(0x0000ff); // Highlight color
-    }
-    else{
-
-    }
-
+    const intersects = raycaster.intersectObjects(scene.children);
 
     for (let i = 0; i < cubes.length; i++){
         cubes[i].rotation.x += 0.01;
         cubes[i].rotation.y += 0.01;
+        cubes[i].material.color.set(0xff0000)
     };
 
-    renderer.render(scene, camera);
-    
-    
-}
+
+    for (let i = 0; i < intersects.length; i++){
+        intersects[i].object.material.color.set(0x0000ff); // Highlight color
+        };
+
+
+    renderer.render(scene, camera);   
+};
 
 animate()
